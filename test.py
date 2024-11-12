48cdad8ad0d4b4eb40ed5ab3431a5025f31d7288
kylym.99 <kylymbekmuratbekov302@gmail.com>
2024-11-12 11:41:54 +0600

    dfghjkl

---
+++ test.py
@@ -0,0 +1,21 @@
+
+
+deys = int(input("введите любую числу: '1-7' : "))
+if deys == 1:
+    print("понедельник")
+elif deys == 2:
+    print("вторник")
+elif deys == 3:
+    print("среда")
+elif deys == 4:
+    print("четверг")
+elif deys == 5:
+    print("пятница")
+elif deys == 6:
+    print("суббота")
+elif deys == 7:
+    print("воскресенье")
+
+
+
+
